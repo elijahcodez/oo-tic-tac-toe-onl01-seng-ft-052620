a2769056745f726c94690bf51c6e8b6c6a7fbe12
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-05 19:39:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,5 +132,18 @@ class TicTacToe
       nil
     end
   end
+  
+  def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
+end
 
 end
