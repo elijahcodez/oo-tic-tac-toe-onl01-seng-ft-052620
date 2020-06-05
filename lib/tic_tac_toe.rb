75c03918290548b84bdeabde6c19516482b1f167
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-05 19:42:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,18 +4,6 @@ class TicTacToe
      @board = board || Array.new(9, " ")
   end
 
-  def play
-    @board = Array.new(9, " ")
-
-    turn until over?
-
-    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
- 
-    puts "Would you like to play again? (Y or N)"
-
-    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
-  end
-
   def turn
     puts "Please enter 1-9:"
     input = gets.strip
